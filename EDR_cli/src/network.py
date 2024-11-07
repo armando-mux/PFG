e6e26b644eb55bf02da143ffec1104ad433af83e
armando-mux <63009962+armando-mux@users.noreply.github.com>
2024-11-07 19:01:20 +0100

    new network script

--- EDR_cli/src/network.py
+++ EDR_cli/src/network.py
@@ -1,23 +1,25 @@
 import signal
 import pyshark
+import pyshark.packet
+import pyshark.packet.fields
+import pyshark.packet.packet
 
 # Configura la interfaz de red para la captura en vivo
 INTERFACE = 'Wi-Fi'  # Cambia 'Ethernet' al nombre de tu interfaz de red
-
 def show_packet(packet):
-    try:
+   try:
         # Obtiene detalles del paquete
         src_ip = packet.ip.src if hasattr(packet, 'ip') else "N/A"
         dst_ip = packet.ip.dst if hasattr(packet, 'ip') else "N/A"
         protocol = packet.highest_layer  # Protocolo de alto nivel
         length = packet.length  # Tamaño del paquete
+        date = packet.frame_info.time_epoch
 
         # Muestra la información en la terminal
-        print(f"Paquete capturado - Origen: {src_ip}, Destino: {dst_ip}, Protocolo: {protocol}, Tamaño: {length} bytes")
-        
-    except AttributeError:
-        # Algunos paquetes pueden no tener los atributos esperados
-        print("Paquete sin datos relevantes, omitiendo...")
+        print(f"Paquete capturado - Origen: {src_ip}, Destino: {dst_ip}, Protocolo: {protocol}, Tamaño: {length} bytes, Fecha: {date}")
+   except AttributeError:
+       print()    
+
         
 
 # Iniciar la captura en vivo en la interfaz especificada
