e4598a3930448bc8a0241aa9fad50ec063e2b317
armando-mux <63009962+armando-mux@users.noreply.github.com>
2025-02-19 16:35:50 +0100

    cambios

---
+++ EDR_cli/src/OS_metrics/linux/directories_monitor.py
@@ -0,0 +1,56 @@
+import datetime
+import os
+from watchdog.observers import Observer
+from watchdog.events import FileSystemEventHandler
+import time
+import csv
+
+
+CSV_FILE = "filesystem_event.csv"
+
+if not os.path.exists(CSV_FILE):
+    with open(CSV_FILE, mode="w", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        writer.writerow(["Date", "Time", "Event", "Path src", "Path Dst", "Archive", "isDirectory"])
+    
+
+excluded = "./"
+
+class CustomEventHandler(FileSystemEventHandler):
+    
+    def log_event(self, isdirectory, event_type, src_path, dest_path=""):
+        file_name = os.path.basename(src_path)  # Obtener solo el nombre del archivo
+        date_time = datetime.datetime.now().strftime("%Y-%m-%d,%H:%M:%S").split(",")  # Obtener fecha y hora
+
+        with open(CSV_FILE, mode="a", newline="", encoding="utf-8") as file:
+            writer = csv.writer(file)
+            writer.writerow([date_time[0], date_time[1], event_type, src_path, dest_path, file_name, isdirectory])
+        
+    def on_modified(self, event):
+        if not(event.src_path.startswith(excluded)):
+            self.log_event(event.is_directory, "MODIFIED", event.src_path)
+
+    def on_created(self, event):
+        self.log_event(event.is_directory, "CREATED", event.src_path)
+
+    def on_deleted(self, event):
+        self.log_event(event.is_directory, "DELETED", event.src_path)
+        
+    def on_moved(self, event):
+        self.log_event(event.is_directory, "MOVED", event.src_path, event.dest_path)  
+
+paths = ["/home/", "/usr/bin/", "/tmp/", "/var/tmp/", "/mnt/" ]
+
+event_handler = CustomEventHandler()
+observer = Observer()
+
+for path in paths:
+    observer.schedule(event_handler, path, recursive=True)
+
+try:
+    observer.start()
+    while True:
+        time.sleep(1)  # Mantén el proceso corriendo
+except KeyboardInterrupt:
+    observer.stop()
+observer.join()
